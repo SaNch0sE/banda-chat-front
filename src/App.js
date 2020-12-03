425e9aa3891f6f118ead663c5af477c6d058d715
ProximaOlder <apion149@gmail.com>
2020-12-03 15:29:02 +0200

    created header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Header from './Header';
+import './styles/index.css';
 
 function App() {
   return (
     <div className="wrapper">
       <Header />
-      <h1>
-        Somthing about messenger
-      </h1>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import IncLogo from './Logo';
-import Blogo from '../public/backLogo.png'
-import './index.css'
+import React from "react";
+import IconMoon from "./MiddleIcon";
 
-function Header () {
+function Header() {
   return (
-    <div className = "Hlogo">
-      <IncLogo />
-      <img src = {"Blogo"}></img>
-      <input className = "ILogin" placeholder = "Login"></input>
-    </div>
-  )
+    <header>
+      <div className="Hlogo">
+        <div className="HText">
+          <p className="text1">See</p>
+          <p className="text2">u</p>
+          <p className="text3">Live</p>
+        </div>
+        <div className="MiddleIcon">
+          <IconMoon />
+        </div>
+        <div className="Hright">
+          <input className="InputLogin" placeholder="Login"></input>
+          <input
+            className="InputPassword"
+            placeholder="&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;"
+          ></input>
+          <a href="#">Forgot your password?</a>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
